23c49d5ef9514c32b3a8528a3f6411f0a11f8392
danishkodemonkey <humberjini@hotmail.com>
2024-03-25 13:21:54 +0100

    Fix: fixed health calculation to work with new shipType attribute

--- src/ship/ship.js
+++ src/ship/ship.js
@@ -1,7 +1,7 @@
 export default class Ship {
 	constructor(shipType) {
 		this.shipType = shipType
-		this.size = this.checkShipType(shipType)
+		this.size = this.checkShipType(this.shipType)
 		this.hits = 0
 		this.health = this.updateHealth()
 	}
@@ -20,18 +20,19 @@ export default class Ship {
 				return 4
 
 			case 'carrier':
+				console.log('returning carrier')
 				return 5
 		}
 	}
 	updateHealth() {
-		this.health = Math.max(0, this.size - this.hits)
+		return Math.max(0, this.size - this.hits)
 	}
 	isSunk() {
 		return this.health === 0 ? true : false
 	}
 	hit() {
 		this.hits += 1
-		this.updateHealth()
+		this.health = this.updateHealth()
 		this.isSunk()
 		return this.hits
 	}
--- src/ship/ship.test.js
+++ src/ship/ship.test.js
@@ -3,17 +3,31 @@ import Ship from './ship'
 describe('ship class methods', () => {
 	let testShip = new Ship('submarine')
 	describe('creating a ship', () => {
+		let carrierTest = new Ship('carrier')
+		let battleshipTest = new Ship('battleship')
+		let destroyerTest = new Ship('destroyer')
+		let submarineTest = new Ship('submarine')
+		let patrolBoatTest = new Ship('patrolBoat')
 		it('creating a ship should be done per ship class', () => {
-			let carrierTest = new Ship('carrier')
-			let battleshipTest = new Ship('battleship')
-			let destroyerTest = new Ship('destroyer')
-			let submarineTest = new Ship('submarine')
-			let patrolBoatTest = new Ship('patrolBoat')
 			expect(carrierTest.shipType).toBe('carrier')
 			expect(battleshipTest.shipType).toBe('battleship')
 			expect(destroyerTest.shipType).toBe('destroyer')
 			expect(submarineTest.shipType).toBe('submarine')
 			expect(patrolBoatTest.shipType).toBe('patrolBoat')
+		})
+		it('created ships should have appropriate sizes', () => {
+			expect(carrierTest.size).toBe(5)
+			expect(battleshipTest.size).toBe(4)
+			expect(destroyerTest.size).toBe(3)
+			expect(submarineTest.size).toBe(3)
+			expect(patrolBoatTest.size).toBe(2)
+		})
+		it('created ships should have appropriate health scores', () => {
+			expect(carrierTest.health).toBe(5)
+			expect(battleshipTest.health).toBe(4)
+			expect(destroyerTest.health).toBe(3)
+			expect(submarineTest.health).toBe(3)
+			expect(patrolBoatTest.health).toBe(2)
 		})
 	})
 	describe('hit() method', () => {
