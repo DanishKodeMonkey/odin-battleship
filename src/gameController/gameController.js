508a1ce0eadab7969170f3629927451680d7d7c3
danishkodemonkey <humberjini@hotmail.com>
2024-03-26 12:15:33 +0100

    feat: add player two NPC check and start condition check

--- src/gameController/gameController.js
+++ src/gameController/gameController.js
@@ -1,16 +1,39 @@
 import { Player, createNPC } from '../players/players'
 
-const gameController = (playerOneName, playerTwoName = createNPC()) => {
+const gameController = (playerOneName, playerTwoName) => {
+	const checkIfNPC = () => {
+		if (playerTwoName) {
+			return new Player(playerTwoName)
+		} else {
+			return createNPC()
+		}
+	}
 	const PlayerOne = new Player(playerOneName)
-	const PlayerTwo = playerTwoName
+	const PlayerTwo = checkIfNPC()
 
-	getPlayerName = () => {
+	const isReady = () => {
+		const playerOneReady = PlayerOne.board.isReady
+		const playerTwoReady = PlayerTwo.board.isReady
+
+		console.log(playerOneReady, playerTwoReady)
+		return playerOneReady && playerTwoReady
+	}
+	const getPlayerName = () => {
 		return PlayerOne.name
+	}
+
+	const start = () => {
+		if (isReady()) {
+			return true // start game condition
+		} else {
+			return false // game not ready to start
+		}
 	}
 	return {
 		PlayerOne,
 		PlayerTwo,
 		getPlayerName,
+		start,
 	}
 }
 
--- src/gameController/gameController.test.js
+++ src/gameController/gameController.test.js
@@ -1,18 +1,34 @@
 import gameController from './gameController'
+import { populateNPCgameBoard } from '../players/players'
 
 describe('gameController', () => {
-	let playerName = 'player One'
-	const game = gameController(playerName)
-
-	it('should initialize using the input of a player name', () => {
-		expect(game.PlayerOne.name).toBe(playerName)
+	let game
+	beforeEach(() => {
+		game = gameController('Player One', 'NPC')
 	})
+
 	it('should initialize two players, playerOne and NPC', () => {
-		expect(game.PlayerOne.name).toBe(playerName)
+		expect(game.PlayerOne.name).toBe('Player One')
 		expect(game.PlayerTwo.name).toBe('NPC')
 	})
 	it('should initialize players as Player class objects', () => {
 		expect(typeof game.PlayerOne).toBe('object')
 		expect(typeof game.PlayerTwo).toBe('object')
+	})
+	it('should wait for both player one, and player two boards to be ready', () => {
+		game.PlayerOne.board.isReady = false
+		game.PlayerTwo.board.isReady = true
+		console.log(game.PlayerOne.board.isReady, game.PlayerTwo.board.isReady)
+		// check if game is ready to start
+		const gameReady = game.start()
+
+		expect(gameReady).toBe(false)
+	})
+	it('should start game when both players are ready', () => {
+		game.PlayerOne.board.isReady = true
+		game.PlayerTwo.board.isReady = true
+
+		const gameReady = game.start()
+		expect(gameReady).toBe(true)
 	})
 })
